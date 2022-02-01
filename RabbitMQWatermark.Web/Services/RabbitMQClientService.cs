31fb0c5f685a031a91dfafe4750b5e494541f64a
buğrahan saygıcı <bsaygici@hotmail.com>
2022-02-01 18:49:07 +0300

    Add project files.

---
+++ RabbitMQWatermark.Web/Services/RabbitMQClientService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQWatermark.Web.Services
+{
+    public class RabbitMQClientService : IDisposable
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection; 
+        private IModel _channel;
+        public static string ExchangeName = "ImageDirectExchange";
+        public static string RountingWatermark = "watermark-route-image";
+        public static string QueueName = "queue-watermark-image";
+
+        private readonly ILogger<RabbitMQClientService> _logger;
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+        }
+
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+            if (_channel.IsOpen == true)
+                return _channel;
+            _channel = _connection.CreateModel();
+
+            _channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct, true, false);
+
+            _channel.QueueDeclare(QueueName, true, false, false);
+
+            _channel.QueueBind(QueueName, ExchangeName, RountingWatermark);
+
+            _logger.LogInformation("RabbitMQ ile bağlantı kuruldu.");
+
+            return _channel;
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel?.Dispose();
+
+            _connection?.Close();
+            _connection?.Dispose();
+
+            _logger.LogInformation("RabbitMQ ile bağlantı koptu.");
+        }
+    }
+}
---
+++ RabbitMQWatermark.Web/Services/RabbitMQPublisher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RabbitMQWatermark.Web.Services
+{
+    public class RabbitMQPublisher
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+
+        public RabbitMQPublisher(RabbitMQClientService rabbitMQClientService)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+        }
+        public void Publish(ProductImageCreatedEvent productImageCreatedEvent)
+        {
+            var channel = _rabbitMQClientService.Connect();
+            var bodyString = JsonSerializer.Serialize(productImageCreatedEvent);
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+        }
+    }
+}
